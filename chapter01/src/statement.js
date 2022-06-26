4327b90a60a2ba857afef06e9b8d2d61c5fbe49e
ogdoyun <dhr8820@gmail.com>
2022-06-26 16:03:20 +0900

    refactor: move fucn totalAmount, totalVolumeCredits to statement

--- chapter01/src/statement.js
+++ chapter01/src/statement.js
@@ -2,7 +2,26 @@ export function statement(invoice, plays) {
 	const statementData = {};
 	statementData.customer = invoice.customer;
 	statementData.performances = invoice.performances.map(enrichPerformance);
+	statementData.totalAmount = totalAmount(statementData);
+	statementData.totalVolumeCredits = totalVolumeCredits(statementData);
 	return renderPlainText(statementData, plays)
+
+	function totalAmount(data) {
+		let result = 0;
+		for (let perf of data.performances) {
+			result += perf.amount;
+		}
+		return result;
+	}
+
+	function totalVolumeCredits(data) {
+		let result = 0;
+		for (let perf of data.performances) {
+			result += perf.volumeCredits;
+		}
+		return result;
+	
+	}
 
 	function enrichPerformance(aPerformance) {
 		const result = Object.assign({}, aPerformance);
@@ -51,8 +70,8 @@ export function statement(invoice, plays) {
 		for (let perf of data.performances) {
 			result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
 		}
-		result += `총액: ${usd(totalAmount())}\n`; // 변수 인라인 후 함수 이름 바꾸기
-		result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+		result += `총액: ${usd(data.totalAmount)}\n`; // 변수 인라인 후 함수 이름 바꾸기
+		result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
 		return result;
 
 		function usd(aNumber) {
@@ -60,22 +79,6 @@ export function statement(invoice, plays) {
 		
 		}
 
-		function totalVolumeCredits() {
-			let result = 0;
-			for (let perf of data.performances) {
-				result += perf.volumeCredits;
-			}
-			return result;
-		
-		}
-
-		function totalAmount() {
-			let result = 0;
-			for (let perf of data.performances) {
-				result += perf.amount;
-			}
-			return result;
-		}
 	}
 
 }
