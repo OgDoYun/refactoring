9ae7095d5e39e479c2b7182012ec3b6f7c8760d9
ogdoyun <dhr8820@gmail.com>
2022-07-02 16:12:34 +0900

    feat: export html file

--- chapter01/src/test/test.test.js
+++ chapter01/src/test/test.test.js
@@ -1,5 +1,6 @@
-import { statement } from './../statement.js'
+import { statement, htmlStatement } from './../statement.js'
 import { plays } from './plays.js'
 import { invoices } from './invoices.js'
 
 console.log(statement(invoices[0], plays));
+console.log(htmlStatement(invoices[0], plays));
