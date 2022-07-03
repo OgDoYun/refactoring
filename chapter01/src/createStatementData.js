996432fa9784255d151cb02f3990f11712147f48
ogdoyun <dhr8820@gmail.com>
2022-07-03 12:08:25 +0900

    refactor: 함수클래스로 옮긴 후 인라인

--- chapter01/src/createStatementData.js
+++ chapter01/src/createStatementData.js
@@ -25,6 +25,14 @@ class PerformanceCalculator {
 		}
 		return result;
 	}
+
+	get volumeCredits() {
+		let result = 0;
+		result += Math.max(this.performance.audience - 30, 0);
+		// 희극 관객 5명마다 추가 포인트를 제공한다.
+		if("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+		return result;
+	}
 }
 
 export default function createStatementData(invoice, plays) {
@@ -40,7 +48,7 @@ export default function createStatementData(invoice, plays) {
 		const result = Object.assign({}, aPerformance);
 		result.play = calculator.play;
 		result.amount = calculator.amount;
-		result.volumeCredits = volumeCreditsFor(result);
+		result.volumeCredits = calculator.volumeCredits;
 		// result example
 		// {
 		// "playID": "hamlet",
@@ -54,19 +62,6 @@ export default function createStatementData(invoice, plays) {
 
 	function playFor(aPerformance) {
 		return  plays[aPerformance.playID]
-	}
-
-	function amountFor(aPerformance) {
-		return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-
-	}
-
-	function volumeCreditsFor(aPerformance) {
-		let result = 0;
-		result += Math.max(aPerformance.audience - 30, 0);
-		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-		return result;
 	}
 
 	function totalAmount(data) {
